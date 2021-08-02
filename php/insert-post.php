c916e2726b6c55dda5e36a7d948f8bd26286d8e2
levintoo <tooklevin@gmail.com>
2021-08-02 12:03:53 +0300

    form action defined

--- php/insert-post.php
+++ php/insert-post.php
@@ -1,12 +1,14 @@
 <?php
-    $title = $_POST['title'];
-    $seo = $_POST['seo'];
-    $content = $_POST['content'];
-    $category = $_POST['category']; // example 1 2
+
+echo("page works");
+    // $title = $_POST['title'];
+    // $seo = $_POST['seo'];
+    // $content = $_POST['content'];
+    // $category = $_POST['category']; // example 1 2
 
 
-    $db = mysql_connect("localhost", "user", "pass", "themesale");
+    // $db = mysql_connect("localhost7882", "root", "", "themesale");
 
-    $query = "INSERT INTO post(title, seo_title, content, author) VALUES ($title, $seo, $content, $category)";
-    mysql_exec($db, $query);
+    // $query = "INSERT INTO post(title, seo_title, content, author) VALUES ($title, $seo, $content, $category)";
+    // mysql_exec($db, $query);
 ?>

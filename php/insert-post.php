93aec00c3900b5d57547f0463644951dde5a637f
levintoo <tooklevin@gmail.com>
2021-08-02 21:21:50 +0300

    db connect

--- php/insert-post.php
+++ php/insert-post.php
@@ -1,14 +1,15 @@
 <?php
 
-echo("page works");
-    // $title = $_POST['title'];
-    // $seo = $_POST['seo'];
-    // $content = $_POST['content'];
-    // $category = $_POST['category']; // example 1 2
 
+    $title = $_POST['title'];
+    $seo = $_POST['seotitle'];
+    $content = $_POST['content'];
+    $category = $_POST['category']; // example 1 2
+    $author = $_POST['author'];
+  
+    echo $title;
+    $db = mysqli_connect("localhost", "root", "", "themesell");
 
-    // $db = mysql_connect("localhost7882", "root", "", "themesale");
-
-    // $query = "INSERT INTO post(title, seo_title, content, author) VALUES ($title, $seo, $content, $category)";
-    // mysql_exec($db, $query);
+    $query = "INSERT INTO post(title, seo_title, content, author) VALUES ('$title', '$seo', '$content', '$author')";
+    mysqli_query($db, $query);
 ?>
